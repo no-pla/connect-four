917c061fc1bf1aa297c5706e640d777ad416e1b3
no-pla <songjihyun323@gmail.com>
2024-06-03 17:07:07 +0900

    test: add landing page test cases

---
+++ src/tests/landing.test.tsx
@@ -0,0 +1,60 @@
+import { beforeEach, describe, expect, it } from "vitest";
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import Index from "../landing";
+
+describe("랜딩 페이지 테스트", () => {
+  describe("요소 랜더링 테스트", () => {
+    beforeEach(() => {
+      render(<Index />);
+    });
+
+    it("버튼이 모두 렌더링되는지 테스트한다", () => {
+      const buttons = screen.getByRole("button");
+      expect(buttons).length(2);
+    });
+
+    it("로고가 올바르게 렌더링되는지 테스트한다", () => {
+      const logo = screen.getByAltText(/로고/i);
+      expect(logo).toBeInTheDocument();
+    });
+  });
+
+  describe("플로우 테스트", () => {
+    it("첫 번째 버튼 클릭 시 게임 페이지로 이동하는지 테스트한다", async () => {
+      render(<Index />);
+      const gameStartButton = screen.getByLabelText(/시작/i);
+      await act(async () => {
+        fireEvent.click(gameStartButton);
+      });
+      expect(global.window.location.pathname).toContain("/game");
+    });
+  });
+
+  describe("모달 테스트", () => {
+    it("두 번째 버튼 클릭 시 게임 설명 모달이 표시되는지 테스트한다", async () => {
+      const { container } = render(<Index />);
+      const modalButton = container.querySelector("#modal_button")!;
+      await act(async () => {
+        fireEvent.click(modalButton);
+      });
+      const modal = container.querySelector("#modal");
+      expect(modal).toBeInTheDocument();
+    });
+
+    it("모달 닫기 버튼을 클릭 시 모달이 화면에서 사라지는지 테스트한다.", async () => {
+      const { container } = render(<Index />);
+
+      const modalButton = container.querySelector("#modal_button")!;
+      await act(async () => {
+        fireEvent.click(modalButton);
+      });
+
+      const modal = container.querySelector("#modal");
+      const modalCloseButton = container.querySelector("#modal_close_button")!;
+      await act(async () => {
+        fireEvent.click(modalCloseButton);
+      });
+      expect(modal).not.toBeInTheDocument();
+    });
+  });
+});
