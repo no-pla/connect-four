e401b079517ac194359e7112a050a3cc7f56cccd
no-pla <songjihyun323@gmail.com>
2024-06-07 15:51:28 +0900

    feat: add routing and add game and 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Index from "./pages/index.tsx";
+import NotFound from "./pages/not-found.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/game",
+    element: <Index />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
