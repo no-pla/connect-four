a83ff41c9383beddc38d24c0e64e2c007f01e3c0
no-pla <songjihyun323@gmail.com>
2024-07-30 14:29:22 +0900

    feat: add not found page

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -1,6 +1,34 @@
+import Container from "components/Container";
+import Button from "components/Custom/Button";
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const goBackHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="bg-darkPurple h-screen flex items-center justify-center">
+      <Container primary={false}>
+        <div className="flex justify-center items-center flex-col gap-[80px] px-4 py-10">
+          <h1 className="text-[1.5rem] font-bold text-center">
+            이 페이지에는
+            <br />
+            아무것도 없어요. 🥺
+          </h1>
+          <Button
+            primary={false}
+            secondary
+            text="메인 화면으로 가기"
+            style="text-center"
+            onClick={() => goBackHome()}
+          />
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default NotFound;
-// TODO: 이것도 해야함
